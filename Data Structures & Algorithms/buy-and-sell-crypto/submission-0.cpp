6c11537d00801c1c357c7a06ee61709c277fbbd4
Anusha25-dev <asanusha25@gmail.com>
2026-08-08 08:35:25 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        //unordered_map<int, int> val; //price, index
+        int maxProfit=0;
+        // for(int i=0;i<prices.size();i++){
+        //     val[prices[i]]=i;
+        // }
+        for(int i=0;i<prices.size();i++){
+            int profit=0;
+            for(int j=i+1;j<prices.size();j++){
+                if(prices[j]>prices[i])
+                    profit=prices[j]-prices[i];
+                maxProfit=max(profit,maxProfit);
+            }
+        }
+        return maxProfit;
+    }
+};
